08292937d7671a3245848c821a5b0536e43c9fa1
rkroko2 <rkroko2@illinois.edu>
2021-02-05 23:10:45 -0600

    A* single done

--- mp1/search.py
+++ mp1/search.py
@@ -19,6 +19,8 @@ files and classes when code is run, so be careful to not modify anything else.
 # searchMethod is the search method specified by --method flag (bfs,dfs,astar,astar_multi,fast)
 
 from time import sleep
+import heapq as pq
+
 
 def bfs(maze):
     """
@@ -37,25 +39,23 @@ def bfs(maze):
     frontier_cell = bfs_frontier.pop(0)
     cur_cell = frontier_cell[0]
     while cur_cell not in maze.waypoints:
-        # check if cost lower than current cost in explored
+        # check if cost higher than current cost in explored
         if cur_cell in bfs_explored.keys():
-            if frontier_cell[2] < bfs_explored[cur_cell][1]:
-                # if cost lower, then update explored set
-                bfs_explored[cur_cell] = (frontier_cell[1], frontier_cell[2])
-            else:
-                # else the cost is higher and it has been explored, so continue
+            if frontier_cell[2] >= bfs_explored[cur_cell][1]:
                 frontier_cell = bfs_frontier.pop(0)
                 cur_cell = frontier_cell[0]
                 continue
-        # if not in the explored set, add it
+        # if not in the explored set, add it (or update it if cost lower)
         bfs_explored[cur_cell] = (frontier_cell[1], frontier_cell[2])
         # add neighbors to frontier
         neighbors = maze.neighbors(cur_cell[0], cur_cell[1])
         for cell in neighbors:
-            bfs_frontier.append((cell, cur_cell, frontier_cell[2]+1))
+            bfs_frontier.append((cell, cur_cell, frontier_cell[2] + 1))
+        # iterate to next in frontier
         frontier_cell = bfs_frontier.pop(0)
         cur_cell = frontier_cell[0]
 
+    # build the path from the parent nodes in the explored sets
     path = [cur_cell]
     cur_parent = frontier_cell[1]
     while cur_parent != maze.start:
@@ -66,6 +66,17 @@ def bfs(maze):
 
     return path
 
+
+def heuristic(cur, goal):
+    """
+    Computes the manhattan distance between a given point and the goal
+    @param cur: Point #1 (tuple)
+    @param goal: Point #2 (tuple)
+    @return distance: the distance b/w Point #1 and Point #2 (int)
+    """
+    return abs(cur[0] - goal[0]) + abs(cur[1] - goal[1])
+
+
 def astar_single(maze):
     """
     Runs A star for part 2 of the assignment.
@@ -74,7 +85,54 @@ def astar_single(maze):
 
     @return path: a list of tuples containing the coordinates of each state in the computed path
     """
-    return []
+    # vairables for search
+    cur_cell = maze.start
+    maze_goal = maze.waypoints[0]
+    frontier = []  # priority queue of (cost+heuristic, (node, parent))
+    explored = {cur_cell: (None, 0)}  # maps ID -> (parent, cost+heuristic)
+
+    # add all the neighbors of the start to the frontier
+    for cell in maze.neighbors(cur_cell[0], cur_cell[1]):
+        g_plus_h = 1 + heuristic(cell, maze_goal)
+        pq.heappush(frontier, (g_plus_h, (cell, cur_cell)))
+
+    # iterate to first node in frontier
+    frontier_node = pq.heappop(frontier)
+    cur_cell = frontier_node[1][0]
+
+    # continue to iterate until goal is reached
+    while cur_cell != maze_goal:
+
+        # compute cost + heuristic for the current cell
+        new_total_c = frontier_node[0]
+
+        # if this cell has been explored, and the explored cost+heuristic is higher, then skip
+        if cur_cell in explored.keys():
+            old_total_c = explored[cur_cell][1]
+            if new_total_c >= old_total_c:
+                frontier_node = pq.heappop(frontier)
+                cur_cell = frontier_node[1][0]
+                continue
+
+        explored[cur_cell] = (frontier_node[1][1], new_total_c)
+        # add/update neighbors in frontier
+        neighbors = maze.neighbors(cur_cell[0], cur_cell[1])
+        for cell in neighbors:
+            total_c = frontier_node[0]+1+heuristic(cell, maze_goal)
+            pq.heappush(frontier, (total_c, (cell, cur_cell)))
+        frontier_node = pq.heappop(frontier)
+        cur_cell = frontier_node[1][0]
+
+    path = [cur_cell]
+    cur_parent = frontier_node[1][1]
+    while cur_parent != maze.start:
+        path.insert(0, cur_parent)
+        explored_tuple = explored[cur_parent]
+        cur_parent = explored_tuple[0]
+    path.insert(0, cur_parent)
+
+    return path
+
 
 def astar_corner(maze):
     """
@@ -85,6 +143,7 @@ def astar_corner(maze):
     @return path: a list of tuples containing the coordinates of each state in the computed path
         """
     return []
+
 
 def astar_multiple(maze):
     """
@@ -97,6 +156,7 @@ def astar_multiple(maze):
     """
     return []
 
+
 def fast(maze):
     """
     Runs suboptimal search algorithm for part 5.
@@ -106,5 +166,3 @@ def fast(maze):
     @return path: a list of tuples containing the coordinates of each state in the computed path
     """
     return []
-    
-            
