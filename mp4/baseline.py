0a2fcd3b1d2d7b3b7c33953a7bb1366e30c4cd40
Robbie Krokos <rkrokos2725@gmail.com>
2021-03-31 16:13:15 -0500

    part1 done

--- mp4/baseline.py
+++ mp4/baseline.py
@@ -13,6 +13,8 @@
 Part 1: Simple baseline that only uses word statistics to predict tags
 """
 
+import operator
+
 def baseline(train, test):
     '''
     input:  training data (list of sentences, with tags on the words)
@@ -20,5 +22,30 @@ def baseline(train, test):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    
-    return []
+    pred = {}
+    total_label_ct = {}
+    for sentence in train:
+        for pair in sentence:
+            cur_count_dict = pred.setdefault(pair[0], {})
+            label_ct = cur_count_dict.get(pair[1], 0)
+            cur_count_dict[pair[1]] = label_ct+1
+            pred[pair[0]] = cur_count_dict
+
+            label_ct_total = total_label_ct.setdefault(pair[1], 0)
+            total_label_ct[pair[1]] = label_ct_total+1
+
+    test_labels = []
+    # Found this utility code for getting the key with max value in a dictionary at:
+    # https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+    most_often_tag = max(total_label_ct.items(), key=operator.itemgetter(1))[0]
+
+    for sentence in test:
+        test_labels.append([])
+        sentence_idx = len(test_labels)-1
+        for word in sentence:
+            if word in pred.keys():
+                tag_ct = pred[word]
+                test_labels[sentence_idx].append((word, max(tag_ct.items(), key=operator.itemgetter(1))[0]))
+            else:
+                test_labels[sentence_idx].append((word, most_often_tag))
+    return test_labels
